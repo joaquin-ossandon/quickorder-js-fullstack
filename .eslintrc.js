bd243a8e7c14b95ebcd0d5b2d43584575b2cb19b
joaquin-ossandon <jossandonmindhubweb@gmail.com>
2025-11-20 01:26:54 -0300

    [QOJS-62]: Agregar Contributing.md y archivos de convención (#12)
    * refactor: remove unused CSS and JavaScript dependencies
    
    * chore: add ESLint and Prettier configuration files, and update contribution guidelines
    
    ---------
    
    Co-authored-by: Joaquin Ossandon <jossandonminhubweb@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  // Configuración de entorno
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+
+  // Configuración de extendidos/presets
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'prettier', 
+  ],
+
+  // Opciones del parser para ES
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+
+  // Reglas personalizadas
+  rules: {
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'camelcase': [
+      'error',
+      {
+        properties: 'never',
+        allow: ['^UNSAFE_'],
+      },
+    ],
+    'id-match': [
+      'error',
+      '^[a-z]+([A-Z][a-z]+)*$',
+      {
+        properties: true,
+        onlyDeclarations: true,
+        ignoreDestructuring: true,
+      },
+    ],
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'no-alert': 'error',
+    'prefer-arrow-callback': 'error',
+    'arrow-parens': ['error', 'as-needed'],
+  },
+
+  // Configuración de Overrides
+  overrides: [
+    {
+      files: ['**/*.config.js', '**/constants.js', '**/enums.js'],
+      rules: {
+        'id-match': 'off',
+        'no-underscore-dangle': 'off',
+      },
+    },
+  ],
+};
