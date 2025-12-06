3aa0c22dea2e099e94dc2fc8d549b19f9fa89451
Joaquin Ossandon <jossandonminhubweb@gmail.com>
2025-12-05 23:50:24 -0300

    refactor: improve HTML structure for product cards in renderProducts function

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,33 @@ function renderProducts() {
   productsContainer.innerHTML = ''; // Limpia el contenedor
 
   products.forEach((product) => {
-    const card = `
-        <div class="col">
-          <div class="card h-100">
-            <img src="${product.imagen}" class="card-img-top img-fit" alt="${
-      product.nombre
-    }" />
+    const card = html`
+      <div class="col">
+        <div class="card h-100">
+          <img
+            src="${product.imagen}"
+            class="card-img-top img-fit"
+            alt="${product.nombre}"
+          />
 
-            <div class="card-body justify-content-around d-flex flex-column">
-              <h5 class="card-title fw-bold mb-1">${product.nombre}</h5>
+          <div class="card-body justify-content-around d-flex flex-column">
+            <h5 class="card-title fw-bold mb-1">${product.nombre}</h5>
 
-              <h6 class="card-subtitle mb-2 text-body-secondary">
-                $${product.precio.toLocaleString()}
-              </h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary">
+              $${product.precio.toLocaleString()}
+            </h6>
 
-              <p class="card-text line-clamp-2" title="${product.descripcion}">
-                ${product.descripcion}
-              </p>
+            <p class="card-text line-clamp-2" title="${product.descripcion}">
+              ${product.descripcion}
+            </p>
 
-              <button class="btn btn-primary w-100 py-2 justify-content-end">
-                <i class="bi bi-cart-plus me-2"></i> Añadir al Carrito
-              </button>
-            </div>
+            <button class="btn btn-primary w-100 py-2 justify-content-end">
+              <i class="bi bi-cart-plus me-2"></i> Añadir al Carrito
+            </button>
           </div>
         </div>
-      `;
+      </div>
+    `;
 
     productsContainer.innerHTML += card;
   });
